1543cf586848022dbb9c943bf28194ac451cc1a5
muradab <se.murad.abdella@gmail.com>
2024-03-26 22:53:42 +0300

    feat: added sidebar

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -3,10 +3,12 @@ import { Button } from "@/components/ui/button";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import { Plus } from "lucide-react";
 import Link from 'next/link'
+import { MobileSidebar } from "./mobile-sidebar";
 
 export const Navbar = ()=>{
     return (
 <nav className="fixed z-50 px-4 top-0 w-full h-14 border-b shadow-sm bg-white flex items-center">
+    <MobileSidebar />
     <div className="flex items-center gap-x-4">
         <div className="hidden md:flex">
             <Logo/>
