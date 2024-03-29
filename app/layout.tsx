93f3579b83de6446c5dade5aed7274ef7c40f42c
muradab <se.murad.abdella@gmail.com>
2024-03-29 22:44:15 +0300

    implement list form form

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,10 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-        {inter.className}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
